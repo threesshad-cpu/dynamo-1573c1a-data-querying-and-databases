68125a8d2cc5b565cac7a126a3c29605a1b621bd
threesshad-cpu <threesshad@gmail.com>
2026-08-16 10:36:02 +0530

    chore: remove obsolete traps and stale artifacts to reduce debug friction

--- check.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('task/data/manufacturing.db')
-print(conn.execute("SELECT * FROM parts WHERE part_id='L100'").fetchall())
-print(conn.execute("SELECT * FROM parts WHERE part_id='SA100'").fetchall())
